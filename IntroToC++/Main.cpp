89955db3b6f613bb152a529c89e73188ae1cb696
Holiday Course <holiday@ad.aie.edu>
2018-08-13 16:23:32 -0500

    Add project files.

---
+++ IntroToC++/Main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Thomas Melancon" << std::endl;
+	std::cout << "Bananas" << std::endl;
+	std::cout << "Apple" << std::endl;
+	std::cout << "Orange" << std::endl;
+	std::cout << "01.Country Boy Can Survive" << std::endl;
+    std::cout << "02.Hurricane" << std::endl;
+	std::cout << "03.Boys Around Here" << std::endl;
+	std::cout << "04.First Day Out" << std::endl;
+	std::cout << "05.Monster" << std::endl;
+	std::cout << "06.Drip or Drown" << std::endl;
+	std::cout << "07.Street Life" << std::endl;
+	std::cout << "08. 100 Shots" << std::endl;
+	std::cout << "09.Brxnks Truck" << std::endl;
+	std::cout << "10. 6th Sense" << std::endl;
+	
+	std::cout << std::endl;
+
+	std::cout << "0" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "2" << std::endl;
+	std::cout << "3" << std::endl;
+	std::cout << "4" << std::endl;
+	std::cout << "5" << std::endl;
+	
+	std::cout << std::endl;
+
+	std::cout << "5" << std::endl;
+	std::cout << "4" << std::endl;
+	std::cout << "3" << std::endl;
+	std::cout << "2" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "0" << std::endl; 
+	system("pause");
+	return 0;
+}
